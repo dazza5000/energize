3fed81709f1fc5bb0ce58545e62b7a51ae6b9b59
Darran Kelinske <darran.kelinske@gmail.com>
2016-07-02 11:14:12 -0500

    various updates

---
+++ app/src/androidTest/java/ly/generalassemb/energize/DrinksActivityTest.java
@@ -0,0 +1,35 @@
+package ly.generalassemb.energize;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.pressKey;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+/**
+ * Created by darrankelinske on 7/1/16.
+ */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class DrinksActivityTest {
+
+    @Rule
+    public ActivityTestRule<DrinksActivity> mActivityTestRule =
+            new ActivityTestRule<>(DrinksActivity.class);
+
+    @Test
+    public void searchDrinkTest() throws Exception {
+
+        onView(withId(R.id.search)).perform(click());
+        onView(withId(android.support.design.R.id.search_src_text))
+                .perform(typeText("red"), pressKey(66));
+    }
+}
---
+++ app/src/main/java/ly/generalassemb/energize/ShoppingCart.java
@@ -0,0 +1,34 @@
+package ly.generalassemb.energize;
+
+import java.util.ArrayList;
+
+import ly.generalassemb.energize.data.Drink;
+
+/**
+ * Created by darrankelinske on 7/1/16.
+ */
+public class ShoppingCart {
+    //Define the Zoo object
+    private static ShoppingCart cart;
+
+    //Definition of the list to hold the animals
+    private static ArrayList<Drink> drinks;
+
+    private ShoppingCart(){
+        drinks = new ArrayList<>();
+    }
+
+    public static ShoppingCart getInstance(){
+        if(cart == null)
+            cart = new ShoppingCart();
+        return cart;
+    }
+
+    public void addDrink(Drink drink){
+        drinks.add(drink);
+    }
+
+    public ArrayList<Drink> getDrinks(){
+        return drinks;
+    }
+}
